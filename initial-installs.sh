8db927690f9c5b0ed11c2674900991191dfab95f
Nati Bekele <nwtbekele@gmail.com>
2026-02-22 21:22:09 -0600

    refactoring script

--- initial-installs.sh
+++ initial-installs.sh
@@ -14,75 +14,91 @@ install_apt () {
         "pavucontrol"
         "solaar"
         "exuberant-ctags"
+        "bat"
         "ripgrep"
+        "terminator"
     )
 
     apt_packages_str="${apt_packages[*]}"
     echo "Installing with apt: ""$apt_packages_str"
     sudo apt install "$apt_packages_str"
 }
-# install vim-plug
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-snap_packages=(
-    "docker"
-    "snapcraft"
-    "pyright"
-    "node"
-    "lxd"
-    "firefox"
-    "htop"
-)
-
-if [[ "$*" =~ ^--personal$ ]]; then
-    snap_packages+=(
-        "whatsie"
-        "steam"
-        "obs-studio"
+snap_installs () {
+    snap_packages=(
+        "docker"
+        "snapcraft"
+        "pyright"
+        "lxd"
+        "firefox"
+        "htop"
     )
-fi
 
-snap_packages_str="${snap_packages[*]}"
-echo "Installing with snap: ""$snap_packages_str"
-for app in "${snap_packages[@]}"; do
-    sudo snap install "$app"
-done
+    if [[ "$*" =~ ^--personal$ ]]; then
+        snap_packages+=(
+            "whatsie"
+            "steam"
+            "obs-studio"
+        )
+    fi
 
-snap_classic_packages=(
-    "code"
-    "astral-uv"
-    "go"
-    "pyright"
-    "rustup"
-)
-echo -e "\n\nInstalling with snap --classic: ""${snap_classic_packages[*]}"
-for app in "${snap_classic_packages[@]}"; do
-    sudo snap install "$app" --classic
-done
-rustup default stable
+    # install go, docker
+    echo -e "\n\nInstalling with snap: ""${snap_packages[*]}"
+    sudo snap install "${snap_packages[@]}"
+
+    # install vs-code
+    snap_classic_packages=(
+        "code"
+        "astral-uv"
+        "go"
+        "pyright"
+        "rustup"
+        "node"
+    )
+    echo -e "\n\nInstalling with snap --classic: ""${snap_classic_packages[*]}"
+    for app in "${snap_classic_packages[@]}"; do
+        sudo snap install "$app" --classic
+    done
+
+    rustup default stable
+}
 
 # install starship
 # get nerd font https://dev.to/pulkitsingh/install-nerd-fonts-or-any-fonts-easily-in-linux-2e3l
-download_font() {
+download_font () {
     echo -e "\n\nDownloading nerd font: Noto"
     TEMP_DIR=$(mktemp -d)
     wget -O "$TEMP_DIR/font.zip" https://github.com/ryanoasis/nerd-fonts/releases/download/v3.4.0/Noto.zip
     unzip "$TEMP_DIR/font.zip" -d $TEMP_DIR
-    sudo mv "$TEMP_DIR"/*.{ttf} /usr/local/share/fonts/
+    sudo mv "$TEMP_DIR"/*.ttf /usr/local/share/fonts/
     fc-cache -f -v
     rm -rf "$TEMP_DIR"
 }
-download_font
-#
-# Installing fzf
-git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install
 
-echo -e "\n\nInstalling starship"
-curl -sS https://starship.rs/install.sh | sh
+config_other () {
+    # install vim-plug
+    echo -e "\n\nInstalling vim plug"
+    curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 
-echo -e "\n\nCopying config files"
-ln -sf ~/moiConfig/.vimrc ~/.vimrc
-ln -sf ~/moiConfig/.bashrc ~/.bashrc
-ln -sf ~/moiConfig/.bash_aliases ~/.bash_aliases
-ln -sf ~/moiConfig/starship.toml ~/.config/starship.toml
+    # Installing fzf
+    echo -e "\n\nInstalling fzf"
+    git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf && ~/.fzf/install
 
+    echo -e "\n\nInstalling starship"
+    curl -sS https://starship.rs/install.sh | sh
+
+    echo -e "\n\nCopying config files"
+    ln -sf ~/moiConfig/.vimrc ~/.vimrc
+    ln -sf ~/moiConfig/.bashrc ~/.bashrc
+    ln -sf ~/moiConfig/.bash_aliases ~/.bash_aliases
+    ln -sf ~/moiConfig/starship.toml ~/.config/starship.toml
+}
+
+
+case $- in
+    *i*) ;;
+    *) return ;;
+esac
+
+if [[ "$*" =~ ^--personal$ ]]; then
+fi
